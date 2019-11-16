da69f76d77e28ef483ab3d26feca09e4bb00cd8e
qingcaier <1721119157@qq.com>
2019-11-16 09:50:41 +0800

    佛了

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -37,6 +37,10 @@ const mutations = {
   setMp(state, mp) {
     state.mp = mp;
   },
+
+  setGroup(state, group) {
+    state.group = group;
+  },
   setFq(state, fq) {
     state.fq = fq;
   },
@@ -117,40 +121,7 @@ const mutations = {
   /**
    *   后台管理端
    */
-  // 改变在残枝培育页面时显示的时间地点按钮内容
-  setActivity(state, activity) {
-    state.activity = activity;
-  },
 
-  // 设置日历发起请求渲染有活动日期
-  setCalendarShowActivity(state, calendarShowActivity) {
-    state.calendarShowActivity = calendarShowActivity;
-  },
-
-  // 缓存当前创建的下水作业对应的活动
-  setNowDivingActivitiesList(state, data) {
-    state.nowDivingActivitiesList.push(data);
-  },
-
-  // 缓存当前的活动编号
-  setNowDivingActivity(state, data) {
-    state.nowDivingActivity = data;
-  },
-
-  // 缓存当前页面对应的活动类型导航栏id
-  setActiveId(state, id) {
-    state.activeId = id;
-  },
-
-  // 缓存当前活动下对应的植株档案列表
-  setActivityFiles(state, filesList) {
-    state.activityFiles = filesList;
-  },
-
-  // 缓存当前正在操作的植株档案
-  setOperateFile(state, fileName) {
-    state.operateFile = fileName;
-  },
   /**
    *  登陆部分
    */
@@ -188,73 +159,6 @@ const mutations = {
   /**
    *   后台管理端
    */
-
-  // 设置当前创建的作业时间戳
-  setCurrentWork(state, timestamp) {
-    state.currentWork = timestamp;
-  },
-
-  // 缓存当前创建的下水作业对应的活动
-  setNowDivingActivitiesList(state, data) {
-    state.nowDivingActivitiesList.push(data);
-  },
-
-  // 缓存当前的活动编号
-  setNowDivingActivity(state, data) {
-    state.nowDivingActivity = data;
-  },
-
-  // 缓存当前页面对应的活动类型导航栏id
-  setActiveId(state, id) {
-    state.activeId = id;
-  },
-
-  // 缓存当前活动下对应的植株档案列表
-  setActivityFiles(state, filesList) {
-    state.activityFiles = filesList;
-  },
-
-  // 缓存当前正在操作的植株档案
-  setOperateFile(state, fileName) {
-    state.operateFile = fileName;
-  },
-
-  setUserInforFromAppVue(state, infor) {
-    state.userInformation = infor;
-  },
-
-  setDropdownKey1(state, key) {
-    state.dropdownKey1 = key;
-  },
-  setDropdownKey2(state, key) {
-    state.dropdownKey2 = key;
-  },
-  setDropdownKey3(state, key) {
-    state.dropdownKey3 = key;
-  },
-  setArea(state, area) {
-    state.area = area;
-  },
-  setMp(state, mp) {
-    state.mp = mp;
-  },
-  setGroup(state, group) {
-    state.group = group;
-  },
-  setCoralNumberStatistic(state, coralNumberStatistic) {
-    state.coralNumberStatistic = coralNumberStatistic;
-  },
-
-  uploadV(state) {
-    state.uploadVisiable = true;
-  },
-  uploadH(state) {
-    state.uploadVisiable = false;
-  },
-
-  /**
-   *   后台管理端
-   */
   // 改变在残枝培育页面时显示的时间地点按钮内容
   setActivity(state, activity) {
     state.activity = activity;
@@ -270,43 +174,6 @@ const mutations = {
     state.nowDivingActivitiesList.push(data);
   },
 
-  // 缓存当前的活动编号
-  setNowDivingActivity(state, data) {
-    state.nowDivingActivity = data;
-  },
-
-  // 缓存当前页面对应的活动类型导航栏id
-  setActiveId(state, id) {
-    state.activeId = id;
-  },
-
-  // 缓存当前活动下对应的植株档案列表
-  setActivityFiles(state, filesList) {
-    state.activityFiles = filesList;
-  },
-
-  // 缓存当前正在操作的植株档案
-  setOperateFile(state, fileName) {
-    state.operateFile = fileName;
-  },
-  /**
-   *  登陆部分
-   */
-  //设置登陆状态
-  setIsLogin(state, bo) {
-    state.isLogin = bo;
-  },
-  //设置培育站点
-  setPYZD(state, arr) {
-    // console.log(arr);
-    state.PYZD = arr;
-  },
-
-  // 缓存当前选择的站点编号
-  setCurrentZD(state, addressNum) {
-    state.currentZD = addressNum;
-  },
-
   /**
    *   后台管理端
    */
@@ -314,11 +181,6 @@ const mutations = {
   // 设置当前创建的作业时间戳
   setCurrentWork(state, timestamp) {
     state.currentWork = timestamp;
-  },
-
-  // 缓存当前创建的下水作业对应的活动
-  setNowDivingActivitiesList(state, data) {
-    state.nowDivingActivitiesList.push(data);
   },
 
   // 缓存当前的活动编号
